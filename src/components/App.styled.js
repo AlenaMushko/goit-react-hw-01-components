6cca91dda1e636e2d5c5f7863803b5f095c27dcf
alena <evstafienko_a@ukr.net>
2023-01-28 16:34:53 +0200

    App.styled

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.section`
   max-width: 1200px;
   margin: 0 auto;
   padding: 24px;
   font-size: 20px;
-  background-color:lightgray;
+  background-color: #0000ff14;
 `;
